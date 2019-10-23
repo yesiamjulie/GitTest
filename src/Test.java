095ca351c9fa026841e54c932cd934d2caadbc33
yesiamjulie <56542349+yesiamjulie@users.noreply.github.com>
2019-10-23 21:45:44 +0900

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,8 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello Java with Git");
+                
+        System.out.println("Additional Edit");
 
     }
 }
