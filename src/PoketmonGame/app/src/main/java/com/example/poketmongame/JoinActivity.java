74b4295b93614bffaa17f4e609ca544d95aa5aa8
yesiamjulie <yesiamjulie0@gmail.com>
2019-11-21 21:59:29 +0900

    29th : JNI test (c++)

--- src/PoketmonGame/app/src/main/java/com/example/poketmongame/JoinActivity.java
+++ src/PoketmonGame/app/src/main/java/com/example/poketmongame/JoinActivity.java
@@ -1,6 +1,6 @@
 package com.example.poketmongame;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,4 +11,11 @@ public class JoinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
     }
-}
+
+
+//    Spring +  postgreSQL = 웹 연동 DB
+
+    //SQLite : 앱에서 가볍게 쓸 수 있는 1인용 앱
+    //firebase : 구글 클라우드에 놓고 가져와서 쓰는 방법
+
+    }
